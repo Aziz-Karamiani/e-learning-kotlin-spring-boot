9258cc45f68d377edf949693e7eea9475dae21b2
a.karamiani <aziz.karamiani@gmail.com>
2023-05-09 18:19:24 +0330

    Initial Commit

---
+++ src/main/kotlin/com/e/learning/elearning/ELearningApplication.kt
@@ -0,0 +1,11 @@
+package com.e.learning.elearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ELearningApplication
+
+fun main(args: Array<String>) {
+    runApplication<ELearningApplication>(*args)
+}
