5d4cfcbc6f534d5451eaf757ee0748e27cb0da9d
a.karamiani <aziz.karamiani@gmail.com>
2023-05-10 10:35:27 +0330

    Setup multiple profile for different Environment

--- src/main/kotlin/com/e/learning/elearning/services/GreetingService.kt
+++ src/main/kotlin/com/e/learning/elearning/services/GreetingService.kt
@@ -1,8 +1,12 @@
 package com.e.learning.elearning.services
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
-    fun sayGreeting(name: String): String = "Hello $name"
+
+    @Value("\${message}")
+    lateinit var message: String
+    fun sayGreeting(name: String): String = "$name, $message"
 }
