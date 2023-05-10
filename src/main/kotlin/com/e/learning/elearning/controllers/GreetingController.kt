fe320c5a12dcc3fc40e9f0bfa2ffd40da9b5b070
a.karamiani <aziz.karamiani@gmail.com>
2023-05-10 12:50:34 +0330

    Setup Logging

--- src/main/kotlin/com/e/learning/elearning/controllers/GreetingController.kt
+++ src/main/kotlin/com/e/learning/elearning/controllers/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.e.learning.elearning.controllers
 
 import com.e.learning.elearning.services.GreetingService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/greeting")
 class GreetingController(val greetingService: GreetingService) {
+    companion object : KLogging()
+
     @GetMapping("/{name}")
-    fun sayGreeting(@PathVariable("name") name: String): String = greetingService.sayGreeting(name)
+    fun sayGreeting(@PathVariable("name") name: String): String {
+        logger.info { "Hello $name" }
+        return greetingService.sayGreeting(name)
+    }
 }
