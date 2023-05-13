47b8fb7da8b72783e35c2f5034cd62dee58510bd
a.karamiani <aziz.karamiani@gmail.com>
2023-05-13 19:49:52 +0330

    Retrieve All Courses

--- src/main/kotlin/com/e/learning/elearning/controllers/CourseController.kt
+++ src/main/kotlin/com/e/learning/elearning/controllers/CourseController.kt
@@ -3,6 +3,7 @@ package com.e.learning.elearning.controllers
 import com.e.learning.elearning.dto.CourseDTO
 import com.e.learning.elearning.services.CourseService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,5 +17,10 @@ class CourseController(val courseService: CourseService) {
     @ResponseStatus(HttpStatus.CREATED)
     fun saveCourse(@RequestBody courseDTO: CourseDTO): CourseDTO {
         return courseService.saveCourse(courseDTO)
+    }
+
+    @GetMapping
+    fun retrieveAllCourses(): List<CourseDTO> {
+        return courseService.retrieveAllCourses()
     }
 }
