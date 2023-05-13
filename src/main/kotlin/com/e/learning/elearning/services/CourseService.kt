f30f735b117fd311a64666af395b927c48fb9f61
a.karamiani <aziz.karamiani@gmail.com>
2023-05-13 19:06:04 +0330

    Course Controller

---
+++ src/main/kotlin/com/e/learning/elearning/services/CourseService.kt
@@ -0,0 +1,26 @@
+package com.e.learning.elearning.services
+
+import com.e.learning.elearning.dto.CourseDTO
+import com.e.learning.elearning.entity.Course
+import com.e.learning.elearning.repository.CourseRepository
+import mu.KLogging
+import org.springframework.stereotype.Service
+
+@Service
+class CourseService(val courseRepository: CourseRepository) {
+    companion object : KLogging()
+
+    fun saveCourse(courseDTO: CourseDTO): CourseDTO {
+        val courseEntity = courseDTO.let {
+            Course(null, it.name, it.category)
+        }
+
+        courseRepository.save(courseEntity)
+
+        logger.info("Saved Course is : $courseEntity")
+
+        return courseEntity.let {
+            CourseDTO(it.id, it.name, it.category)
+        }
+    }
+}
